859bf5960a838921b1e4142ba122a4cf6355329a
CachesKCH <gfdominguez119@gmail.com>
2020-01-11 12:59:17 -0300

    bot.py reescrito en mejor sintaxis

--- bot.py
+++ bot.py
@@ -1,21 +1,34 @@
 from secret import token, secret_server_id
+import random
 import discord
+from discord.ext import commands
 
-p_var_server_id = secret_server_id
-
-client = discord.Client()
+p_var_server_id = secret_server_id  # Guarda el server ID secreto en una variable publica que puede ser reusada
+client = commands.Bot(command_prefix=".")  # Define el prefix con el cual los comandos van a ser llamados
 
 
 @client.event
-async def on_message(message):
-    print(message.content)
-    server_id = client.get_guild(439556519311441920)
-    channels = ["comandos", "general"]
+async def on_ready():
+    print("Bot is ready.")
 
-    if str(message.channel) in channels:
-        if message.content.find("!ligma") != -1:
-            await message.channel.send("fav")
-        elif message.content == "!users":
-            await message.channel.send(f"""# de Miembros {server_id.member_count}""")
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! el bot respondio en {round(client.latency * 1000)} ms")
+
+
+@client.command(aliases=["8ball"])
+async def _8ball(ctx, *, question):
+    responses = ["si bro", "no bro"]
+    await ctx.send(f"Pregunta: {question}\nRespuesta: {random.choice(responses)}")
+
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} se ha unido al servidor.")
+
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} ha salido del servidor.")
 client.run(token)
--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,4 @@
-def imprime_input(entrada):
-    print(entrada)
+x = 0
+while True:
+    x += 1
 
-
-imprime_input(input("Escribe STR"))
-
-
-@imprime_input
-def suma_cosas(num1, num2):
-    result = num1 + num2
-    print(result)
