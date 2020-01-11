f55e5d65fc0b7deb1ff8c53a5dfe5cfbc07d1690
CachesKCH <gfdominguez119@gmail.com>
2020-01-10 21:44:42 -0300

    probando comandos

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
-from secret import token
+from secret import token, secret_server_id
 import discord
+
+p_var_server_id = secret_server_id
 
 client = discord.Client()
 
@@ -7,7 +9,13 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     print(message.content)
-    if message.content.find("!hello") != -1:
-        await message.channel.send("Hi")
-        
+    server_id = client.get_guild(439556519311441920)
+    channels = ["comandos", "general"]
+
+    if str(message.channel) in channels:
+        if message.content.find("!ligma") != -1:
+            await message.channel.send("fav")
+        elif message.content == "!users":
+            await message.channel.send(f"""# de Miembros {server_id.member_count}""")
+
 client.run(token)
